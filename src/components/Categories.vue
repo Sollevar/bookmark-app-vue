1e55d22aea8e16eed1dd7d5b6d1a21ecdc9dcbf3
Igor Atlasyuk <sollevar@mail.ru>
2026-04-07 17:49:59 +0700

    added create category

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,39 +1,64 @@
 <script setup lang="ts">
 import { useCategoryStore } from '@/stores/categories.store';
 import { onMounted } from 'vue';
+import AddCategoryIcon from './icons/AddCategoryIcon.vue';
 
 const store = useCategoryStore();
 onMounted(() => {
     store.fetchCategories();
 })
+
 </script>
 
 <template>
-    <ul class="category__list list-reset">
-        <li class="category__item" v-for="category in store.categories" :key="category.id">
-            <a :href="`${category.alias}`">{{ category.name}}</a>
-        </li>
-    </ul>
+    <div class="category">
+        <ul class="category__list list-reset">
+            <li class="category__item" v-for="category in store.categories" :key="category.id">
+                <RouterLink active-class="category__active" :to="`/main/${category.alias}`">{{ category.name }}
+                </RouterLink>
+            </li>
+        </ul>
+        <AddCategoryIcon class="category__icon" @click="store.createCategory()" />
+    </div>
 </template>
 
 <style scoped>
-.category__list{
-    margin-top: 40px;
+
+.category{
+    display: flex;
+    flex-direction: column;
 }
 
-.category__item:not(:last-child){
+.category__list {
+    margin-top: 40px;
+    margin-bottom: 40px;
+}
+
+.category__item:not(:last-child) {
     margin-bottom: 34px;
 }
 
-.category__item a{
+.category__item a {
     font-size: 14px;
     color: var(--card-color);
     text-decoration: none;
     transition: all 0.2s ease-in-out;
 }
 
-.category__item a:hover{
+.category__item a:hover,
+.category__item a.category__active {
     font-size: 24px;
     font-weight: 700;
+}
+
+.category__icon {
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+}
+
+.category__icon:hover{
+    scale: 1.2;
 }
 </style>
--- src/stores/categories.store.ts
+++ src/stores/categories.store.ts
@@ -1,13 +1,22 @@
 import { API_ROUTES, http } from "@/api";
 import type { ICategory } from "@/interfaces/category.interface";
 import { defineStore } from "pinia";
+import { v4 as uuidv4 } from "uuid";
 import { ref } from "vue";
+
 
 export const useCategoryStore = defineStore('categories', () => {
     const categories = ref<ICategory[]>();
     async function fetchCategories() {
         const { data } = await http.get<ICategory[]>(API_ROUTES.categories);
         categories.value = data;
+    };
+    async function createCategory (){
+        const { data } = await http.post<ICategory>(API_ROUTES.categories, {
+            name:'Новая категория',
+            alias: uuidv4(),
+        });
+       categories.value?.push(data)
     }
-    return { categories, fetchCategories }
+    return { categories, fetchCategories, createCategory }
 })
